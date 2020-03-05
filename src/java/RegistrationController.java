16d8869f4c9c32c756eb84083c18f78e713f4fda
Zafrul Hasan Khan (Nasim) <nasimkhan18003@gmail.com>
2020-03-05 22:59:58 +0600

    registration process

---
+++ src/java/RegistrationController.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Monir
+ */
+@WebServlet(name = "RegistrationController", urlPatterns = {"/RegistrationController"})
+public class RegistrationController extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    /**
+     *
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        String firstname = request.getParameter("fname");
+        String lastname = request.getParameter("lname");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String gender = request.getParameter("gender");
+        String email = request.getParameter("email");
+        String phone = request.getParameter("phone");
+        String bloodgroup = request.getParameter("bldgrp");
+        String address = request.getParameter("address");
+        String age = request.getParameter("age");
+
+        String regSuccessAlert = "Registered Successfully!";
+        String regErrorAlert = "Registration Failed!";
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bldms", "root", "");
+            PreparedStatement ps = con.prepareStatement("insert into user values(?,?,?,?,?,?,?,?,?,?,?,?)");
+            ps.setString(1, firstname);
+            ps.setString(2, lastname);
+            ps.setString(3, username);
+            ps.setString(4, password);
+            ps.setString(5, gender);
+            ps.setString(6, email);
+            ps.setString(7, phone);
+            ps.setString(8, bloodgroup);
+            ps.setString(9, address);
+            ps.setInt(10, Integer.parseInt(age));
+            ps.setString(11, "N/A");
+            ps.setString(12, "N/A");
+            int i = ps.executeUpdate();
+            if (i > 0) {
+                request.setAttribute("regSuccessAlert", regSuccessAlert);
+                RequestDispatcher rd = request.getRequestDispatcher("registrationsuccess.jsp");
+                rd.forward(request, response);
+            }
+            ps.close();
+            con.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(RegistrationController.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println(ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(RegistrationController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
---
+++ web/registrationsuccess.jsp
@@ -0,0 +1,30 @@
+<%-- 
+    Document   : registrationsuccess
+    Created on : Aug 2, 2019, 10:19:33 PM
+    Author     : Monir
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <title>Registration Successful</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row text-center" style="float: center" align="center">
+                <div class="col col-sm-offset-4">
+                    <br><br> <h2 style="color:#0fad00">Success</h2>
+                    <img src="images/chktrue.jpg">
+                    <p style="font-size:20px;color:#5C5C5C;">Thank you for registering into our web site. Please update your profile with your details.
+                        Click on below link to login.</p>
+                    <a href="login.jsp" class="btn btn-success">     Log in      </a>
+                    <br><br>
+                </div>
+
+            </div>
+        </div>
+    </body>
+</html>
